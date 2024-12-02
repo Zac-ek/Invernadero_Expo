831c89af6831c08155fa4225a40fefc33f349a98
ZaS-ex <a.zacek.gc@gmail.com>
2024-12-02 11:25:52 -0600

    CArga de navegacion

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import InitScreen from './Views/Init';
+import LoginScreen from './Views/Login';
+import home from './Views/home/home'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <img src='./img/Logo.jpg'></img>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Init">
+        <Stack.Screen name="Init" component={InitScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <Button title='Iniciar Sesion'></Button>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
